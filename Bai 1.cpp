a4c5468b903f879f7a53d29daec4430505a7c802
tanhtanh <73300642+tanhtanh@users.noreply.github.com>
2021-03-06 17:18:46 +0700

    message

--- Bai 1.cpp
+++ Bai 1.cpp
@@ -1,35 +1,38 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-
-bool checkQueen (int x1,int y1,int x2,int y2)
-{
-    if (x1==x2) return true;
-    if (y1==y2) return true;
-    if (abs(x1-x2)==abs(y1-y2)) return true;
-    return false;
-}
-
-int main ()
-{
-    int n;
-    cin >>n;
-    int a[n][2];
-    for (int i=0;i<n;i++)
-    {
-        cin >>a[i][0]>>a[i][1];
-    }
-    for (int i=0;i<n;i++)
-    {
-        for (int j=i+1;j<n;j++)
-        {
-            if(checkQueen(a[i][0],a[i][1],a[j][0],a[j][1])==true)
-            {
-                cout<<"yes";
-                return -1;
+bool checkmate(int a[] , int b[],int n ){
+    for(int i = 0 ; i < n ; i++){
+        for(int j = i + 1 ; j < n ; j++){
+            if(a[i] == a[j]){
+                return true ;
+                break;
+            }
+            if(b[i] == b[j]){
+                return true ;
+                break;
+            }
+            if(abs(a[i] - a[j]) - abs(b[i] - b[j]) == 0){
+                return true ;
+                break;
             }
         }
     }
-    cout<<"no";
+    return false ;
+}
+int main(){
+    int n ;
+    cin >> n ;
+    int a[n+1];
+    int b[n+1];
+    for(int i = 0 ; i < n ; i++){
+        cin >> a[i] >> b[i];
+    }
+    if(checkmate(a,b,n) == true) {
+        cout <<"yes";
+    }
+    else {
+        cout <<"no";
+    }
     return 0;
 }
